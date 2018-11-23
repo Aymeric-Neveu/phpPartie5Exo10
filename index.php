0ed832b0fd5baf2947058e0b81991d6c75e1c41d
root <emploi.neveu@gmail.com>
2018-11-23 14:07:28 +0100

    exo10

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+   <html lang="fr" dir="ltr">
+     <head>
+       <meta charset="utf-8">
+       <title></title>
+     </head>
+     <body>
+       <?php
+   $département = array (
+       69 => 'Nord-Pas-Calais',
+       80 => 'Somme',
+       02 => 'Aisne',
+       59 => 'Nord',
+       60 => 'Oise');
+       foreach ($département as $départementNumber => $départementNumber){
+            echo 'Le département ' . $départementNumber . ' a le numéro ' . $départementNumber;
+          }
+       ?>
+     </body>
+   </html>
